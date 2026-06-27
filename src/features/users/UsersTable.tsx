b323697f5c3c2dea8f925d0c36e0e1d3881bf1d9
nastima <grozdilova@mail.ru>
2026-06-27 18:59:14 +0300

    table users 30

---
+++ src/features/users/UsersTable.tsx
@@ -0,0 +1,42 @@
+import {useGetUsersQuery} from "../../store/api/usersApi.ts";
+import {Table, TableBody, TableCell, TableHead, TableRow, Paper} from '@mui/material';
+import type {User} from "../../store/api/types.ts";
+
+
+export const UsersTable = () => {
+    const {data, isLoading} = useGetUsersQuery();
+
+    if(isLoading) return <div>Loading...</div>
+
+    return (
+        <Paper sx={{padding: 2}}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Age</TableCell>
+                        <TableCell>Company</TableCell>
+                        <TableCell>City</TableCell>
+                    </TableRow>
+                </TableHead>
+
+                <TableBody>
+                    {data?.users?.map((user: User) => (
+                        <TableRow key={user.id}>
+                            <TableCell>{user.id}</TableCell>
+                            <TableCell>
+                                {user.firstName} {user.lastName}
+                            </TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>{user.age}</TableCell>
+                            <TableCell>{user.company?.name}</TableCell>
+                            <TableCell>{user.address?.city}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Paper>
+    );
+};
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,15 +1,5 @@
-import {useGetUsersQuery} from '../store/api/usersApi.ts';
+import {UsersTable} from '../features/users/UsersTable.tsx';
 
 export const DashboardPage = () => {
-    console.log('render dashboard');
-    const result = useGetUsersQuery();
-    console.log(result);
-
-    if (result.isLoading) return <div>Loading...</div>;
-
-    return (
-        <pre>
-            {JSON.stringify(result.data, null, 2)}
-        </pre>
-    )
+    return <UsersTable/>
 }
