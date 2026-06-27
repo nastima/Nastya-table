794d176dbb898be4c1b73f5f20c1a1a2bb9867fb
nastima <grozdilova@mail.ru>
2026-06-27 18:41:14 +0300

    type Users

---
+++ src/store/api/types.ts
@@ -0,0 +1,22 @@
+export interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    age: number;
+    phone: string;
+    image: string;
+    company: {
+        name: string;
+    };
+    address: {
+        city: string;
+    };
+}
+
+export interface UsersResponse {
+    users: User[];
+    total: number;
+    skip: number;
+    limit: number;
+}
--- src/store/api/usersApi.ts
+++ src/store/api/usersApi.ts
@@ -1,14 +1,14 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import type {UsersResponse} from "./types.ts";
 
 export const usersApi = createApi({
     reducerPath: 'usersApi',
-
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://dummyjson.com',
     }),
 
     endpoints: (builder) => ({
-        getUsers: builder.query<any, void>({
+        getUsers: builder.query<UsersResponse, void>({
             query: () => '/users',
         }),
     }),
